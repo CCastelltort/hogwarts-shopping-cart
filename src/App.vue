0fe457b57600a3f9155b841d95879e30090609ef
Carlota Castelltort Pinto <c.castelltortpinto@tcu.edu>
2025-02-24 05:15:22 +0100

    reactivity

--- src/App.vue
+++ src/App.vue
@@ -159,7 +159,7 @@
 <script setup>
 import {ref} from 'vue'
 let username = 'Harry'
-let shoppingCartItems = [
+let shoppingCartItems = ref([
   {
     id: 1,
     productName: 'Dragon Liver',
@@ -200,7 +200,7 @@ let shoppingCartItems = [
     quantity: 1,
     image: 'src/assets/img/Nimbus2000.jpg'
   },
-]
+])
 
 let hideDetails = ref(false)
 
